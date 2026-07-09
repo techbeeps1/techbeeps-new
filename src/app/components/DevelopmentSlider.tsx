333d10cd161c8795c3d3bf0669ce9313e127a413
official-asif-ali <asif@techbeeps.co.in>
2026-07-09 16:47:20 +0530

    update design

--- src/app/components/DevelopmentSlider.tsx
+++ src/app/components/DevelopmentSlider.tsx
@@ -245,7 +245,7 @@ export default function DevelopmentSlider() {
                 </div> 
                 <div className="pt-5 px-2.5 pb-2.5 rounded-b-[16px] bg-[#0d0a1b]/96 z-1 flex-1 flex flex-col justify-between">              
                   <div className="px-4 pt-3 pb-3">
-                    <h3 className="text-[19px] md:text-[21px] font-bold text-white leading-tight">
+                    <h3 className="text-[18px] 2xl:text-[21px] font-bold text-white leading-tight">
                       {card.title}
                     </h3>
                   </div>
